5b9832b5c9e0e07e5e1c926fbcebe8d53948d761
Djura22 <michaeliangroves@gmail.com>
2020-09-10 11:20:25 +0100

    update to app

--- app.rb
+++ app.rb
@@ -1,53 +1,4 @@
 require 'sinatra/base'
-<<<<<<< HEAD
-require 'mongoid'
-Mongoid.load!(File.join(File.dirname(__FILE__), 'config', 'mongoid.yml'))
-
-class Post
-  include Mongoid::Document
-
-  field :title, type: String
-  field :body, type: String
-
-  has_many :comments
-end
-
-class Comment
-  include Mongoid::Document
-
-  field :name, type: String
-  field :message, type: String
-
-  belongs_to :post
-end
-
-class MakersBnb < Sinatra::Base
-  #get '/' do
-  #  'MakersBnB!'
-  #end
-
-  get '/posts' do
-    Post.all.to_json
-  end
-  
-  post '/posts' do
-    post = Post.create!(params[:post])
-    post.to_json
-  end
-  
-  get '/posts/:post_id' do |post_id|
-    post = Post.find(post_id)
-    post.attributes.merge(
-      comments: post.comments,
-    ).to_json
-  end
-  
-  post '/posts/:post_id/comments' do |post_id|
-    post = Post.find(post_id)
-    comment = post.comments.create!(params[:comment])
-    {}.to_json
-  end
-=======
 require_relative './lib/space.rb'
 
 class MakersBnb < Sinatra::Base
@@ -77,6 +28,5 @@ class MakersBnb < Sinatra::Base
   end
 
 
->>>>>>> viewing_spaces
   run! if app_file == $0
 end
